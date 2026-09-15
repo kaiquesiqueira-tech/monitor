8527a00ed8d46f6168f7fc0f9c49c2e2ba02dba9
Kaique alves Siqueira <kaique.siqueira@bautek.com.br>
2026-09-15 15:40:56 -0300

    atualiza base

--- historico.js
+++ historico.js
@@ -1,1 +1,1 @@
-window.HISTORICO=[{"data":"2026-09-15","pedidos":310,"vencidos":279,"valor_vencido":473483.79,"solicitacoes":228,"paradas30":100,"abaixo_pp":363}];
+window.HISTORICO=[{"data":"2026-09-15","pedidos":356,"vencidos":263,"valor_vencido":428991.09,"solicitacoes":228,"paradas30":100,"abaixo_pp":363}];
