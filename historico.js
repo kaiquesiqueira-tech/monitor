ddd93400b9cd097c201979d9b9389bc171ad6a87
Kaique alves Siqueira <kaique.siqueira@bautek.com.br>
2026-09-16 08:40:23 -0300

    atualiza base 16/09/2026 08:40

--- historico.js
+++ historico.js
@@ -1,1 +1,1 @@
-window.HISTORICO=[{"data":"2026-09-15","pedidos":312,"vencidos":284,"valor_vencido":479917.13,"solicitacoes":270,"paradas30":100,"abaixo_pp":363},{"data":"2026-09-16","pedidos":358,"vencidos":278,"valor_vencido":447399.19,"solicitacoes":241,"paradas30":98,"abaixo_pp":376}];
+window.HISTORICO=[{"data":"2026-09-15","pedidos":312,"vencidos":284,"valor_vencido":479917.13,"solicitacoes":270,"paradas30":100,"abaixo_pp":363},{"data":"2026-09-16","pedidos":358,"vencidos":278,"valor_vencido":447399.19,"solicitacoes":241,"paradas30":98,"abaixo_pp":382}];
